5b98a2968c086c03226b8469ca45d9e79581511e
meet <dodiyameet041@gmail.com>
2024-01-17 13:54:29 +0530

    initiated landing page development

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home'
+import Navbar from './components/home/Navbar'
+import './App.css'
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
